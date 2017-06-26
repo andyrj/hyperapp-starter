ad67927cc32891023c4918c5077e87b94038d180
Andy Johnson <andyrjohnson82@gmail.com>
2017-06-25 19:17:56 -0700

    lint clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import app from './app';
 
-//TODO: client specific code will go here
-
 // hyperapp doesn't hydrate existing dom so it must be regenerated
 // this will cause a flicker when re-rendered client side... meh..
 document.body.innerHTML = '';
