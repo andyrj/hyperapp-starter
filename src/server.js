6f1e73afa5e5c72ebf21b64f95ef86fcfc1de6d6
Andy Johnson <andyrjohnson82@gmail.com>
2017-06-29 14:54:36 -0700

    cleaned up duplicate state definition

--- src/server.js
+++ src/server.js
@@ -44,6 +44,7 @@ k.use(async(ctx, next) => {
 	{
 		await next();
 	} else {
+    const state = { count: 0 };
 		const baseDoc = `
 			<!DOCTYPE html>
 			<html lang=en>
@@ -55,13 +56,15 @@ k.use(async(ctx, next) => {
 					<script src='/js/vendor.js' defer></script>
 					<script src='/js/main.js' defer></script>
 					<script id="state" type="application/json">
-						${JSON.stringify({ count: 0 })}
+						${JSON.stringify(state)}
 					</script>
 				</head>
 				<body>
 				</body>
 			</html>
 		`;
+
+    console.log(document.readyState);
 
     /* may still need these for router mixin	
 		global.location = {
@@ -70,7 +73,7 @@ k.use(async(ctx, next) => {
 		// just stubbing out functions not needed for SSR with hyperapp-router
 		global.addEventListener = (str, fn) => {};
     */
-		app({ count: 0 });
+		app(state);
 		
 		ctx.body = serialize(document.body);
 		ctx.type = 'text/html';
